56e9e069eb7b3ced4cb155955f4a911ed822a81f
carnotboulon <arnaudboland@gmail.com>
2016-02-15 22:22:47 +0100

    12 months

--- dataManager.py
+++ dataManager.py
@@ -111,18 +111,18 @@ class dataManager():
         #data["gas"] = [gasData["jan"], gasData["feb"], gasData["mar"], gasData["apr"], gasData["may"], gasData["jun"], gasData["jul"], gasData["aug"], gasData["sep"], gasData["oct"], gasData["nov"], gasData["dec"]] # First element of the list is a tuple.
         #data["elec"] = [elecData["jan"], elecData["feb"], elecData["mar"], elecData["apr"], elecData["may"], elecData["jun"], elecData["jul"], elecData["aug"], elecData["sep"], elecData["oct"], elecData["nov"], elecData["dec"]]
         
-		data["time"] = []
-		data["gas"] = []
-		data["elec"] = []
-		for i in range(1,13):
-			index = currentMonth + i
-			if index > 12:
-				index = index - 12
-			data["gas"].append(dataManager.MONTHS[index])
-			data["gas"].append(gasData[dataManager.MONTHS[index]])
-			data["elec"].append(elecData[dataManager.MONTHS[index]])
-			
-		return data
+        data["time"] = []
+        data["gas"] = []
+        data["elec"] = []
+        for i in range(1,13):
+            index = currentMonth + i
+            if index > 12:
+                index = index - 12
+            data["gas"].append(dataManager.MONTHS[index])
+            data["gas"].append(gasData[dataManager.MONTHS[index]])
+            data["elec"].append(elecData[dataManager.MONTHS[index]])
+		
+        return data
 
     def getLastWeeks(self, numberOfWeeks):
         currentWeek = datetime.datetime.today().isocalendar()[1]
